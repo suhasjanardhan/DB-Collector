cfeb9475de31ef886f9060d77bcd81166ae4c0d4
Satvik Vishnubhatta <satvik.2692@gmail.com>
2018-07-23 15:37:28 -0700

    Added little more functionality

--- Collector/python_API/dbhelper.py
+++ Collector/python_API/dbhelper.py
@@ -1,7 +1,7 @@
 import sqlite3
 from enum import Enum
 
-DB_SWITCH = 1 #SQLITE = 1, MONGO = 2
+DB_SWITCH = 0 #SQLITE = 0, MONGO = 1
 
 class CCHECK_DATA_TYPES(Enum):
     UINT8_T = ('TINYINT', 'INT32'); 
@@ -28,17 +28,44 @@ class Tabledata:
            for instance [("name_of_1st_column", CCHECK_DATA_TYPES.INT), ... ] 
         '''
         self.columns = []
+        self.table_data_dictionary = {}
 
-    def add_column(self, name, data_type): #data_type should be of type CCHECK_DATA_TYPES
+    def add_column(self, name, data_type): 
+        '''
+            name STRING
+            data_type CCHECK_DATA_TYPES
+        '''
         column_tuple = (name, data_type.value[DB_SWITCH]);
         self.columns.append(column_tuple);
 
     def __str__(self):
         pretty_print = "TableName: " + self.table_name + "\nColumns:\n   "
-        for x in self.columns:
-            pretty_print+=x[0]+" "+x[1]+"\n   " 
+        "      ".join(["%s %s\n" % (x[0],x[1]) for x in self.columns]) 
         return pretty_print
 
+    def get_table_info(self):
+        return {
+            Table_Name: self.table_name,
+            Columns: self.columns;
+        }
+    
+
+     def createDataDictionary(self):
+         self.data_dict = list();
+     
+     def addDataToColumn(column_name, column_data):
+         temp_array = [None for col in self.columns]
+         if(self.columns.index(column_name) != -1):
+             temp_array[self.columns.index(column_name)] = column_data;
+             self.data_dict.append((temp_array))
+     
+         else:
+             raise Exception("No such column exists in the table ")
+
+     def addDataToTable(self, **kwargs):
+         self.data_dict.append(kwargs.values())
+
+    
    
      
 print(CCHECK_DATA_TYPES.UINT8_T.value[0]);

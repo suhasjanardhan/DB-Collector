ae1814bd00b3ea3b3954a6355d92cf7da1e81b3f
Satvik Vishnubhatta <satvik.2692@gmail.com>
2018-07-24 16:00:48 -0700

    Adding some changes

--- Collector/python_API/dbhelper.py
+++ Collector/python_API/dbhelper.py
@@ -1,26 +1,25 @@
 import sqlite3
 import coll_python
-from enum import Enum
 
 DB_SWITCH = 0 #SQLITE = 0, MONGO = 1
 
 CCHECK_DATA_TYPES = {
-    'UINT8_T' : ('TINYINT', 'INT32'), 
-    'UINT16_T' : ('SMALLINT', 'INT32'),
-    'INT' : ('INT', 'INT32'),
-    'UNSIGNED_INT' : ('UNSIGNED BIG INT', 'INT32'),
-    'INT32' : ('INT', 'INT32'),
+    'UINT8_T'           : ('TINYINT', 'INT32'), 
+    'UINT16_T'          : ('SMALLINT', 'INT32'),
+    'INT'               : ('INT', 'INT32'),
+    'UNSIGNED_INT'      : ('UNSIGNED BIG INT', 'INT32'),
+    'INT32'             : ('INT', 'INT32'),
     'UNSINGED_LONG_INT' : ('INTEGER', 'INT64'),
-   'STRING' : ('TEXT', 'UTF8'),
-   'CHAR' : ('TEXT', 'UTF8'),
-   'UNSIGNED_CHAR' : ('TEXT', 'UTF8'),
-   'TIMESTAMP' : ('NUMERIC', 'TIMESTAMP'),
+    'STRING'            : ('TEXT', 'UTF8'),
+    'CHAR'              : ('TEXT', 'UTF8'),
+    'UNSIGNED_CHAR'     : ('TEXT', 'UTF8'),
+    'TIMESTAMP'         : ('NUMERIC', 'TIMESTAMP'),
 }
 
 class Tabledata:
     def __init__(self, table_name):
        '''
-           table_name String
+           table_name text
        '''
        self.table_name = table_name 
        '''                       
@@ -33,9 +32,10 @@ class Tabledata:
 
     def add_column(self, name, data_type): 
        '''
-           name STRING
-           data_type CCHECK_DATA_TYPES
+           name      text
+           data_type text
        '''
+
        db_type = CCHECK_DATA_TYPES.get(data_type, None);
        if db_type is None:
            print('***TYPE WARNING : No such type for given '+data_type)
@@ -61,7 +61,7 @@ class Tabledata:
            self.table_data_list.append((temp_array))
      
        else:
-           print(column_name + "No such column exists in the table ")
+           print(column_name + " : No such column exists in the table ")
            exit(0);
 
     def add_data_to_table(self, **kwargs):
@@ -72,19 +72,21 @@ class Tabledata:
        print("calling create table");
        coll_python.CreateTable(self.columns,self.table_name);
    
-     
-sample_table_data = Tabledata("Test_Table")
-sample_table_data.add_column('col1', 'INT');
-sample_table_data.add_column('col2', 'STRING');
-print(sample_table_data);
-sample_table_data.call_create_table();
-sample_table_data.add_data_to_table(col1=1,col2='abc');
-sample_table_data.add_data_to_table(col1=2,col2='sat');
-sample_table_data.add_data_to_table(col1=3,col2='xyz');
-dic = { 'col2':'abc', 'col1': '4'}
-sample_table_data.add_data_to_table(**dic);
-print(sample_table_data.table_data_list)
+def main(): 
+   sample_table_data = Tabledata("Test_Table")
+   sample_table_data.add_column('col1', 'INT');
+   sample_table_data.add_column('col2', 'STRING');
+   print(sample_table_data);
+   sample_table_data.call_create_table();
+   sample_table_data.add_data_to_table(col1=1,col2='abc');
+   sample_table_data.add_data_to_table(col1=2,col2='sat');
+   sample_table_data.add_data_to_table(col1=3,col2='xyz');
+   dic = { 'col2':'abc', 'col1': '4'}
+   sample_table_data.add_data_to_table(**dic);
+   print(sample_table_data.table_data_list)
 
 
+if __name__ == '__main__':
+    main()
  
                 

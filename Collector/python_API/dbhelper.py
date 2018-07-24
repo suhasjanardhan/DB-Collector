56ebd866c40911f0b885463504a939f6f59acc28
Satvik Vishnubhatta <satvik.2692@gmail.com>
2018-07-24 11:23:55 -0700

    modified enum to dictionary

--- Collector/python_API/dbhelper.py
+++ Collector/python_API/dbhelper.py
@@ -4,18 +4,18 @@ from enum import Enum
 
 DB_SWITCH = 0 #SQLITE = 0, MONGO = 1
 
-class CCHECK_DATA_TYPES(Enum):
-    UINT8_T = ('TINYINT', 'INT32'); 
-    UINT16_T = ('SMALLINT', 'INT32');
-    INT = ('INT', 'INT32')
-    UNSIGNED_INT = ('UNSIGNED BIG INT', 'INT32')
-    INT32 = ('INT', 'INT32')
-    UNSINGED_LONG_INT = ('INTEGER', 'INT64')
-    STRING = ('TEXT', 'UTF8')
-    CHAR = ('TEXT', 'UTF8')
-    UNSIGNED_CHAR = ('TEXT', 'UTF8')
-    TIMESTAMP = ('NUMERIC', 'TIMESTAMP')
-        
+CCHECK_DATA_TYPES = {
+    'UINT8_T' : ('TINYINT', 'INT32'), 
+    'UINT16_T' : ('SMALLINT', 'INT32'),
+    'INT' : ('INT', 'INT32'),
+    'UNSIGNED_INT' : ('UNSIGNED BIG INT', 'INT32'),
+    'INT32' : ('INT', 'INT32'),
+    'UNSINGED_LONG_INT' : ('INTEGER', 'INT64'),
+   'STRING' : ('TEXT', 'UTF8'),
+   'CHAR' : ('TEXT', 'UTF8'),
+   'UNSIGNED_CHAR' : ('TEXT', 'UTF8'),
+   'TIMESTAMP' : ('NUMERIC', 'TIMESTAMP'),
+}
 
 class Tabledata:
     def __init__(self, table_name):
@@ -29,14 +29,18 @@ class Tabledata:
           for instance [("name_of_1st_column", CCHECK_DATA_TYPES.INT), ... ] 
        '''
        self.columns = []
-       self.table_data_dictionary = []
+       self.table_data_list = []
 
     def add_column(self, name, data_type): 
        '''
            name STRING
            data_type CCHECK_DATA_TYPES
        '''
-       column_tuple = (name, data_type.value[DB_SWITCH]);
+       db_type = CCHECK_DATA_TYPES.get(data_type, None);
+       if db_type is None:
+           print('***TYPE WARNING : No such type for given '+data_type)
+           db_type = ('TEXT', 'UTF8')
+       column_tuple = (name, db_type[DB_SWITCH]);
        self.columns.append(column_tuple);
 
     def __str__(self):
@@ -51,22 +55,18 @@ class Tabledata:
        }
 
     def add_data_to_column(column_name, column_data):
-       temp_array = [None for col in self.columns]
+       temp_array = [None] * len(self.columns)
        if(self.columns.index(column_name[0]) != -1):
            temp_array[self.columns.index(column_name)] = column_data;
-           self.table_data_dictionary.append((temp_array))
+           self.table_data_list.append((temp_array))
      
        else:
-           raise Exception("No such column exists in the table ")
+           print(column_name + "No such column exists in the table ")
+           exit(0);
 
     def add_data_to_table(self, **kwargs):
-       temp_array = [None] * len(self.columns)
-       temp_col_array = [x[0] for x in self.columns]
-       for k,v in kwargs.items():
-           if(temp_col_array.index(k)!= -1):
-                temp_array[temp_col_array.index(k)] = v
-           
-       self.table_data_dictionary.append(tuple(temp_array))
+        self.table_data_list.append(kwargs)
+       
     
     def call_create_table(self):
        print("calling create table");
@@ -74,8 +74,8 @@ class Tabledata:
    
      
 sample_table_data = Tabledata("Test_Table")
-sample_table_data.add_column('col1', CCHECK_DATA_TYPES.INT);
-sample_table_data.add_column('col2', CCHECK_DATA_TYPES.STRING);
+sample_table_data.add_column('col1', 'INT');
+sample_table_data.add_column('col2', 'STRING');
 print(sample_table_data);
 sample_table_data.call_create_table();
 sample_table_data.add_data_to_table(col1=1,col2='abc');
@@ -83,7 +83,7 @@ sample_table_data.add_data_to_table(col1=2,col2='sat');
 sample_table_data.add_data_to_table(col1=3,col2='xyz');
 dic = { 'col2':'abc', 'col1': '4'}
 sample_table_data.add_data_to_table(**dic);
-print(sample_table_data.table_data_dictionary)
+print(sample_table_data.table_data_list)
 
 
  
